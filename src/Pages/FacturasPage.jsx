61cf83ee958fc177945247d65473a8f24e49aab4
Sunart1 <79371501+Sunart1@users.noreply.github.com>
2023-11-25 17:08:23 -0500

    Nav bar structure done

--- src/Pages/FacturasPage.jsx
+++ src/Pages/FacturasPage.jsx
@@ -1,7 +1,18 @@
+import { navOptions } from "../const/navOptions"
+
 export function FacturasPage() {
-    return(
+    return (
         <>
-        <h1>Facturas</h1>
+            <h1>Facturas</h1>
+            <nav className="absolute bottom-0 w-full py-3 bg-[#f4f4f4]">
+                <ul className="grid grid-cols-3 place-items-center justify-center">
+                    {
+                        navOptions.map(({ label, image }) => {
+                            return <li key={label} className="flex flex-col items-center"><img src={image} alt={label} className="w-1/2 stroke-gray-600"/> {label}</li>
+                        })
+                    }
+                </ul>
+            </nav>
         </>
     )
 }
---
+++ src/const/navOptions.js
@@ -0,0 +1,14 @@
+export const navOptions = [
+    {
+        label: 'Facturas',
+        image: '../src/assets/icons/facturas-icon.svg'
+    },
+    {
+        label: 'Consumo',
+        image: '../src/assets/icons/consumo-icon.svg'
+    },
+    {
+        label: 'Ajustes',
+        image: '../src/assets/icons/ajustes-icon.svg'
+    },
+]
