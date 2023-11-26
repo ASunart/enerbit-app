b6bc0dc6410de852bb437bb49da913960b808abd
Sunart1 <79371501+Sunart1@users.noreply.github.com>
2023-11-25 19:11:40 -0500

    Made cards for selecting place

---
+++ src/components/Card.jsx
@@ -0,0 +1,18 @@
+import { SecondaryButton } from "./SecondaryButton";
+
+export function Card() {
+    const regularText = "font-inter font-regular text-gray-500 text-lg"
+    return (
+        <article className="flex flex-col gap-4 px-5 py-7 shadow-md w-11/12 rounded-xl">
+            <span>
+                <h2 className="font-inter font-extrabold text-orange text-3xl">Mi casa</h2>
+            </span>
+            <span>
+                <h3 className="font-inter font-semibold text-gray-500 text-lg">Calle 6a #43-25</h3>
+                <h4 className={regularText}>Los Cámbulos</h4>
+                <h5 className={regularText}>Cali, Valle del Cauca</h5>
+            </span>
+            <SecondaryButton label='Ver facturas' />
+        </article>
+    )
+}
