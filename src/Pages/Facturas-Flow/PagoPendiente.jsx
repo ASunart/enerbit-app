cd6962c9a35c7a31443f53895e16e38ee0694f34
Sunart1 <79371501+Sunart1@users.noreply.github.com>
2023-11-25 20:25:35 -0500

    Pagos pendientes page structure done

---
+++ src/Pages/Facturas-Flow/PagoPendiente.jsx
@@ -0,0 +1,23 @@
+import { AsistenciaBtn } from "../../components/AsistenciaBtn";
+import { FacturaCard } from "../../components/FacturaCard";
+import { NavBar } from "../../components/NavBar";
+import { SecondaryButton } from "../../components/SecondaryButton";
+
+export function PagoPendiente() {
+    return(
+        <>
+            <header className="w-10/12 items-center mx-auto mt-10">
+                <AsistenciaBtn />
+            </header>
+            <main className="flex flex-col gap-3 w-10/12 mx-auto mt-6">
+            <p className="font-inter font-semibold text-base text-gray-400">Pagos y facturas - <span className="text-purple">Mi casa</span></p>
+                <section className="grid grid-cols-2 gap-6 mt-3">
+                    <SecondaryButton label='Pendientes'/>
+                    <SecondaryButton label='Pagadas'/>
+                </section>
+                <FacturaCard/>
+            </main>
+            <NavBar />
+        </>
+    )
+}
--- src/routers/FacturasRouter.jsx
+++ src/routers/FacturasRouter.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { FacturasPage } from "../Pages/FacturasPage";
 import { DetalleFactura } from "../Pages/Facturas-Flow/DetalleFactura";
 import { RefPagoPage } from "../Pages/Facturas-Flow/RefPagoPage";
+import { PagoPendiente } from "../Pages/Facturas-Flow/PagoPendiente";
 
 export function FacturasRouter() {
     return (
@@ -9,6 +10,10 @@ export function FacturasRouter() {
             <Route
                 path="/"
                 element={<FacturasPage />}
+            />
+            <Route
+                path="/pago-pendiente"
+                element={<PagoPendiente />}
             />
             <Route
                 path="/detalle-factura"
