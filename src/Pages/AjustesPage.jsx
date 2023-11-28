1de9630a346ebf72cbccc440e7ccad741cbda00e
Sunart1 <79371501+Sunart1@users.noreply.github.com>
2023-11-27 19:56:04 -0500

    Start settings page

--- src/Pages/AjustesPage.jsx
+++ src/Pages/AjustesPage.jsx
@@ -1,8 +1,26 @@
+import { NavBar } from "../components/NavBar";
 
 export function AjustesPage() {
     return (
         <>
-            
+        <header className="flex flex-col items-center gap-2">
+            <img src="../src/assets/icons/profile-img.svg" alt="" />
+            <h1>Darío Martinez</h1>
+            <p>Visualización principiante</p>
+        </header>
+            <section>
+                <h2>Configuración</h2>
+                <ul>
+                    <li>Tus grupos</li>
+                </ul>
+            </section>
+            <section>
+                <h2>Soporte</h2>
+                <ul>
+                    <li>Habla con nosotros</li>
+                </ul>
+            </section>
+            <NavBar />
         </>
     )
 }
