ccb35ae76c19d11ff0970ef69b09d490d3cc0ebd
Sunart1 <79371501+Sunart1@users.noreply.github.com>
2023-11-28 12:43:15 -0500

    Make nav bar active icons and text

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,16 +3,19 @@ import { navOptions } from "../const/navOptions"
 import { NavLink } from "react-router-dom"
 
 export function NavBar() {
-
+    
     return (
         <nav className="bottom-0 w-full py-3 bg-[#f4f4f4] fixed">
-            <ul className="grid grid-cols-3 place-items-center justify-center items-center">
+            <ul className="grid grid-cols-3 place-items-center items-center">
                 {
-                    navOptions.map(({ label, image, route }) => {
+                    navOptions.map(({ label, image, route, active }) => {
+                        const textColor = window.location.pathname === route ? 'text-orange' : 'text-gray-400'
+                        const navStyle =`flex flex-col items-center font-inter font-regular ${textColor} text-base gap-2`
+                        const imageColor = window.location.pathname === route ? active : image
                         return (
                             <NavLink key={label} to={route}>
-                                <li className="flex flex-col items-center">
-                                    <img src={image} alt={label} className="w-1/2 stroke-gray-600" />
+                                <li className={navStyle}>
+                                    <img src={imageColor} alt={label} />
                                         {label}
                                 </li>
                             </NavLink>
